36dfa9eb4f366b3f94d13814621c27c4b7e86a9c
Patrik Vaďura <patrikvadura@icloud.com>
2023-05-10 18:58:24 +0200

    update: components, namespace

--- config/structure/components.php
+++ config/structure/components.php
@@ -2,6 +2,16 @@
 
 namespace developtheme\config\structure;
 
+function partials($partials_name) {
+    ob_start();
+    $template_directory = get_template_directory();
+    $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
+    $path = $template_directory . '/' . $path_prefix . 'partials' . '/' . $partials_name . '.php';
+
+    include($path);
+    return ob_get_clean();
+}
+
 function component($component_name, $component_variant, $variables = [], $content_callback = null) {
     extract($variables);
     ob_start();
@@ -12,18 +22,9 @@ function component($component_name, $component_variant, $variables = [], $conten
 
     $template_directory = get_template_directory();
     $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
-    $path = $template_directory . '/' . $path_prefix . 'components/' . $component_name . '/' . $component_variant;
+    $path = $template_directory . '/' . $path_prefix . 'components' . '/' . $component_name . '/' . $component_variant . '.php';
 
-    include(locate_template($path . '.php'));
-    return ob_get_clean();
-}
-
-function partials($partials_name) {
-    ob_start();
-    $template_directory = get_template_directory();
-    $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
-    $path = $template_directory . '/' . $path_prefix . 'partials/' . $partials_name;
-    include(locate_template($path . '.php'));
+    include($path);
     return ob_get_clean();
 }
 
