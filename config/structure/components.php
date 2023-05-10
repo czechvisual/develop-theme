58038c31e57e7c50fba09d13437e461db1e91311
Patrik Vaďura <patrikvadura@icloud.com>
2023-05-10 19:04:18 +0200

    update: components, namespace

--- config/structure/components.php
+++ config/structure/components.php
@@ -2,30 +2,34 @@
 
 namespace developtheme\config\structure;
 
-function partials($partials_name) {
-    ob_start();
-    $template_directory = get_template_directory();
-    $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
-    $path = $template_directory . '/' . $path_prefix . 'partials' . '/' . $partials_name . '.php';
+if (!function_exists('origin_partials')) {
+    function origin_partials($partials_name) {
+        ob_start();
+        $template_directory = get_template_directory();
+        $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
+        $path = $template_directory . '/' . $path_prefix . 'partials' . '/' . $partials_name . '.php';
 
-    include($path);
-    return ob_get_clean();
+        include($path);
+        return ob_get_clean();
+    }
 }
 
-function component($component_name, $component_variant, $variables = [], $content_callback = null) {
-    extract($variables);
-    ob_start();
+if (!function_exists('origin_component')) {
+    function origin_component($component_name, $component_variant, $variables = [], $content_callback = null) {
+        extract($variables);
+        ob_start();
 
-    if ($content_callback !== null) {
-        $template_content = $content_callback();
+        if ($content_callback !== null) {
+            $template_content = $content_callback();
+        }
+
+        $template_directory = get_template_directory();
+        $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
+        $path = $template_directory . '/' . $path_prefix . 'components' . '/' . $component_name . '/' . $component_variant . '.php';
+
+        include($path);
+        return ob_get_clean();
     }
-
-    $template_directory = get_template_directory();
-    $path_prefix = (strpos($template_directory, '/origin') === false) ? 'origin/' : '';
-    $path = $template_directory . '/' . $path_prefix . 'components' . '/' . $component_name . '/' . $component_variant . '.php';
-
-    include($path);
-    return ob_get_clean();
 }
 
 
